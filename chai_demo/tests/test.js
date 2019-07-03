2c21c813d543e458c6e96227396081a3c408a691
Poon Nguyen <nguyenduypoon@gmail.com>
2019-07-03 14:29:35 +0700

    Backup and Restore database when testing

--- chai_demo/tests/test.js
+++ chai_demo/tests/test.js
@@ -6,10 +6,10 @@ import app from '../server';
 chai.use(chaiHttp);
 chai.should();
 
-describe('Students',()=>{
-    describe('GET',()=>{
-        it("should get all students records",(done)=>{
-            chai.request(app).get('/').end((err,res)=>{
+describe('Students', () => {
+    describe('GET', () => {
+        it("should get all students records", (done) => {
+            chai.request(app).get('/').end((err, res) => {
                 res.should.be.a('object');
                 done();
             });
@@ -17,25 +17,25 @@ describe('Students',()=>{
     });
 
     // Test to get single student record
-    it("should get a single student record",async () => {
+    it("should get a single student record", (done) => {
         const id = 1;
-        awake (chai.request(app)
+        chai.request(app)
             .get(`/${id}`)
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('object');
-             });
-        );
+            })
+        done();
     });
-    
-   // Test to get single student record
-   it("should not get a single student record", (done) => {
+
+    // Test to get single student record
+    it("should not get a single student record", (done) => {
         const id = 5;
         chai.request(app)
             .get(`/${id}`)
             .end((err, res) => {
                 res.should.have.status(404);
                 done();
-             });
+            });
     });
 })
